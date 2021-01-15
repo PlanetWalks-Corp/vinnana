cdc33bd7304363cc314f6f0deb3e4999851c4a3f
Pranav <pranav.aggarwal@round.glass>
2021-01-15 13:23:17 +0530

    fixed command line runner

--- dynamicsinglepage/src/main/java/com/planetwalks/dynamicsinglepage/DynamicsinglepageApplication.java
+++ dynamicsinglepage/src/main/java/com/planetwalks/dynamicsinglepage/DynamicsinglepageApplication.java
@@ -1,5 +1,4 @@
 package com.planetwalks.dynamicsinglepage;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.planetwalks.dynamicsinglepage.models.Cities;
 import com.planetwalks.dynamicsinglepage.services.CitiesRepositoriesImpl;
@@ -16,22 +15,28 @@ import org.springframework.context.annotation.Bean;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
 public class DynamicsinglepageApplication extends SpringBootServletInitializer {
+
+	@Autowired
+	CitiesRepositoriesImpl citiesRepositories;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DynamicsinglepageApplication.class, args);
 	}
 	@Bean
-	CommandLineRunner runner(CitiesRepositoriesImpl citiesRepositories) {
+	CommandLineRunner runner() {
 		return args -> {
 			ObjectMapper mapper = new ObjectMapper();
 			TypeReference<List<Cities>> typeReference = new TypeReference<List<Cities>>(){} ;
 			InputStream inputStream = TypeReference.class.getResourceAsStream("/json/city.json");
 			try {
-				List<Cities> city = mapper.readValue(inputStream,typeReference);
-				citiesRepositories.create((Cities) city);
+				List<Cities> cities = mapper.readValue(inputStream,typeReference);
+				for(Cities city : cities){
+					citiesRepositories.create((Cities) city);
+				}
 				System.out.println("Users Saved!");
 			} catch (IOException e){
 				System.out.println("Unable to save users: " + e.getMessage());
