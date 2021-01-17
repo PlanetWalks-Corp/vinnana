dc1969b68e1c6f7b8671eab25162cc3115f1cb4c
sushil kainth <kainths.2206@gmail.com>
2021-01-17 15:01:28 +0530

    Fixed album, fair and person seeding

--- dynamicsinglepage/src/main/java/com/planetwalks/dynamicsinglepage/services/CityRepositoryImpl.java
+++ dynamicsinglepage/src/main/java/com/planetwalks/dynamicsinglepage/services/CityRepositoryImpl.java
@@ -1,9 +1,7 @@
 package com.planetwalks.dynamicsinglepage.services;
 
-import com.planetwalks.dynamicsinglepage.models.City;
-import com.planetwalks.dynamicsinglepage.models.Place;
-import com.planetwalks.dynamicsinglepage.repositories.CityRepository;
-import com.planetwalks.dynamicsinglepage.repositories.PlaceRepository;
+import com.planetwalks.dynamicsinglepage.models.*;
+import com.planetwalks.dynamicsinglepage.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +16,15 @@ public class CityRepositoryImpl {
 
 	@Autowired
 	private PlaceRepository placeRepository;
+
+	@Autowired
+	private FairRepository fairRepository;
+
+	@Autowired
+	private PersonRepository personRepository;
+
+	@Autowired
+	private AlbumRepository albumRepository;
 
 	public List<City> getAll() {
 		return (List<City>) cityRepository.findAll();
@@ -38,6 +45,33 @@ public class CityRepositoryImpl {
 				//}
 			}
 		}
+		if(city.getFairList() != null) {
+			for(Fair fair : city.getFairList()) {
+//				Fair savedFair = fairRepository.findFairByFairNameAndCity(fair.getFairName(), savedInstance).get();
+//				if(savedFair == null) {
+					fair.setCity(savedInstance);
+					fairRepository.save(fair);
+				//}
+			}
+		}
+		if(city.getPersonList() != null) {
+			for(Person person : city.getPersonList()) {
+//				Person savedPerson = personRepository.findPersonByPersonFirstNameAndCity(person.getPersonFirstName(), savedInstance).get();
+//				if(savedPlace == null) {
+					person.setCity(savedInstance);
+					personRepository.save(person);
+				//}
+			}
+		}
+		if(city.getAlbum() != null) {
+			Album album = city.getAlbum();
+//			Album savedAlbum = personRepository.findAlbumByAlbumNameAndCity(album.getAlbumName(), savedInstance).get();
+//			if(savedPlace == null) {
+			album.setCity(savedInstance);
+			albumRepository.save(album);
+		//}
+		}
+
 		 return savedInstance;
 	}
 
