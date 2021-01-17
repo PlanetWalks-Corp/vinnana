df2ea37d28edaba64e436b4a895b06d64c116d61
Tanay <tanaygupta1234@gmail.com>
2021-01-17 20:11:42 +0530

    project done

--- frontendDynamicSinglePage/src/app/citylayout/citylayout.component.ts
+++ frontendDynamicSinglePage/src/app/citylayout/citylayout.component.ts
@@ -3,6 +3,7 @@ import {CityinfoService} from '../cityinfo.service';
 import { ActivatedRoute} from '@angular/router';
 import {Router} from '@angular/router';
 import {City} from '../classes';
+import {Place} from '../classes';
 
 @Component({
   selector: 'app-citylayout',
@@ -15,17 +16,20 @@ export class CitylayoutComponent implements OnInit {
   citi: any=-1;
   place: any=-1;
   person: any=-1;
+  foodPoint :Place[]=[];
+  placesToVisit :Place[]=[];
    constructor(private cityinfo: CityinfoService, private route: ActivatedRoute, private router: Router) {
      this.route.params.subscribe( params => {this.id = params["id"]});
     }
   history = this.citi.history;//"The city of Jaipur was founded by King of Amer, Maharaja Sawai Jai Singh II on November 18, 1727.";
   image_source = 'https://res.cloudinary.com/ksush/image/upload/v1610801552/deviTalab_dzxpmf.jpg';
-  FoodPoint=this.citi.placeList && this.citi.placeList.filter((place:any)=>{place.type==="Food Place"});
-  PlacesToVisit = ['image1','image2', 'image3','image4'];
+  
   FamousPersonalities = this.citi.personList;
   ngOnInit(): void {
   this.cityinfo.getCityInfo(this.id).subscribe((data: any) => {
         this.citi = data;
+        this.foodPoint=this.citi.placeList && this.citi.placeList.filter((place:any)=>place.placeType==="food");
+        this.placesToVisit=this.citi.placeList && this.citi.placeList.filter((place:any)=>place.placeType!=="food");
   })
   }
 
