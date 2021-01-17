1bcfa378a770f8a2eef2df942b26c548252cf37d
sushil kainth <kainths.2206@gmail.com>
2021-01-17 17:01:22 +0530

    added data.sql file for populating the data

--- dynamicsinglepage/src/main/java/com/planetwalks/dynamicsinglepage/services/CityRepositoryImpl.java
+++ dynamicsinglepage/src/main/java/com/planetwalks/dynamicsinglepage/services/CityRepositoryImpl.java
@@ -32,47 +32,48 @@ public class CityRepositoryImpl {
 
 
 	public City create(City city) {
-		City savedInstance = cityRepository.findOneBySlug(city.getSlug()).get();
-		if(savedInstance == null){
-			savedInstance = cityRepository.save(city);
-		}
-		if(city.getPlaceList() != null) {
-			for(Place place : city.getPlaceList()) {
-//				Place savedPlace = placeRepository.findPlaceByPlaceNameAndCity(place.getPlaceName(), savedInstance).get();
-//				if(savedPlace == null) {
-					place.setCity(savedInstance);
-					placeRepository.save(place);
-				//}
-			}
-		}
-		if(city.getFairList() != null) {
-			for(Fair fair : city.getFairList()) {
-//				Fair savedFair = fairRepository.findFairByFairNameAndCity(fair.getFairName(), savedInstance).get();
-//				if(savedFair == null) {
-					fair.setCity(savedInstance);
-					fairRepository.save(fair);
-				//}
-			}
-		}
-		if(city.getPersonList() != null) {
-			for(Person person : city.getPersonList()) {
-//				Person savedPerson = personRepository.findPersonByPersonFirstNameAndCity(person.getPersonFirstName(), savedInstance).get();
-//				if(savedPlace == null) {
-					person.setCity(savedInstance);
-					personRepository.save(person);
-				//}
-			}
-		}
-		if(city.getAlbum() != null) {
-			Album album = city.getAlbum();
-//			Album savedAlbum = personRepository.findAlbumByAlbumNameAndCity(album.getAlbumName(), savedInstance).get();
-//			if(savedPlace == null) {
-			album.setCity(savedInstance);
-			albumRepository.save(album);
-		//}
-		}
-
-		 return savedInstance;
+		return cityRepository.save(city);
+//		City savedInstance = cityRepository.findOneBySlug(city.getSlug()).get();
+//		if(savedInstance == null){
+//			savedInstance = cityRepository.save(city);
+//		}
+//		if(city.getPlaceList() != null) {
+//			for(Place place : city.getPlaceList()) {
+////				Place savedPlace = placeRepository.findPlaceByPlaceNameAndCity(place.getPlaceName(), savedInstance).get();
+////				if(savedPlace == null) {
+//					place.setCity(savedInstance);
+//					placeRepository.save(place);
+//				//}
+//			}
+//		}
+//		if(city.getFairList() != null) {
+//			for(Fair fair : city.getFairList()) {
+////				Fair savedFair = fairRepository.findFairByFairNameAndCity(fair.getFairName(), savedInstance).get();
+////				if(savedFair == null) {
+//					fair.setCity(savedInstance);
+//					fairRepository.save(fair);
+//				//}
+//			}
+//		}
+//		if(city.getPersonList() != null) {
+//			for(Person person : city.getPersonList()) {
+////				Person savedPerson = personRepository.findPersonByPersonFirstNameAndCity(person.getPersonFirstName(), savedInstance).get();
+////				if(savedPlace == null) {
+//					person.setCity(savedInstance);
+//					personRepository.save(person);
+//				//}
+//			}
+//		}
+//		if(city.getAlbum() != null) {
+//			Album album = city.getAlbum();
+////			Album savedAlbum = personRepository.findAlbumByAlbumNameAndCity(album.getAlbumName(), savedInstance).get();
+////			if(savedPlace == null) {
+//			album.setCity(savedInstance);
+//			albumRepository.save(album);
+//		//}
+//		}
+//
+//		 return savedInstance;
 	}
 
 	public Optional<City> findByCityId(Long cityId){
